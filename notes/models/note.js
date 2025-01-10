fb720a6f48d13c698b28c19600d65365832287ae
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-01-10 17:21:33 -0300

    update: project structure

--- notes/models/note.js
+++ notes/models/note.js
@@ -1,27 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-// connection
-const url = process.env.MONGODB_URI_NOTES
-console.log('connecting to MongoDB')
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-// schema
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
-    required: true
+    required: true,
+    minlength: 5
   },
-  important: Boolean
+  important: Boolean,
 })
+
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
