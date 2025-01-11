6fc9e116d98c2fdc0534e01702226e7238e7cb47
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-01-11 10:20:03 -0300

    exercise 4.5

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -63,8 +63,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max)
+}
+
 module.exports = {
   blogsForTest,
   dummy,
   totalLikes,
+  favoriteBlog,
 }
